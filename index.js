bb36320115ce8dcda7f5dc54999f7e0b6214557e
helisonoliveira <helisonoliveira@hotmail.com>
2023-11-24 22:09:06 -0300

    tentando entender e resolver o problema do escopo da classe

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 class hero {
-    constructor(){ (name, age, type)
+    constructor(name, age, type) {
+        
         this.name = name
         this.age = age
-        this.type = type    
-    }
-        attack() {
-            switch (this.type){ 
-                case "mago": "magia";
-                    break;
-                case "guerreiro": "espada";
-                    break;
-                case "monge": "artes marciais";
-                    break;
-                case "ninja": "shuriken";
-                    break;
-            }
+        this.type = type
 
-            console.log(`O ${this.type} atacou usando ${this.attack}`);
+        console.log(`Usuário: ${name} - idade: ${age}`);
+    }
+
+    action(type){
+        switch (type) {
+            case "mago": return "magia"
+                
+            case "guerreiro": return "espada";
+                
+            case "monge": return "artes marciais";
+                
+            case "ninja": return "shuriken";
+                
         }
+    }
+    attack(action) {
+      
+           console.log(`O ${this.type} atacou usando ${action}`);
+    }
 
 }
+let indexHero = Math.floor(Math.random() * 4)
+
+let typeOfHero = ["mago", "guerreiro", "monge", "ninja"]
+
+let type = typeOfHero[indexHero]
+
+//function 
+
+
+let hero1 = new hero("Rael", 30, type)
+
+hero1.attack()
+
